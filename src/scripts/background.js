2d4b1e1c1070d87379b0b822148a4e31c898d57f
sai madhan <saimadhan@outlook.in>
2023-07-10 13:36:15 +0530

    working service worker

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,26 +1,3 @@
-/** @type {Set<string>} */
-const URLS = new Set([
-  "https://twitter.com/*",
-  "https://*.reddit.com/*",
-  "https://instagram.com/*",
-]);
-
-/**
- * @param {number} tabId
- * @param {chrome.tabs.TabChangeInfo} changeInfo
- * @param {chrome.tabs.Tab} tab
- */
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.url && URLS.has(tab.url)) {
-    chrome.pageAction.show(tabId);
-  }
-});
-
-/**
- * @param {chrome.tabs.Tab} tab
- */
-chrome.pageAction.onClicked.addListener((tab) => {
-  chrome.tabs.sendMessage(tab.id, {
-    message: "pageAction clicked",
-  });
-});
+chrome.runtime.onInstalled.addListener(() =>
+  chrome.action.setBadgeText({ text: "OFF" })
+);
