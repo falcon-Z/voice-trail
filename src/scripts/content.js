2615876cfae9a6c92009b8b053dcbc10204a7a1a
sai madhan <saimadhan@outlook.in>
2023-07-12 17:42:43 +0530

    new chrome extension setup

--- src/scripts/content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender) => {
-  if (message.message === "pageAction clicked") {
-    console.log(`Tab ID: ${sender.tab.id}`);
-    console.log(`URL: ${sender.tab.url}`);
-  }
-});
